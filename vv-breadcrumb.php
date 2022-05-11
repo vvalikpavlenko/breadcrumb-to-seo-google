7551a4083b0c619c9e5269dfb2937992fcda298e
Valik Pavlenko <valikpost8@gmail.com>
2022-05-11 13:17:06 +0300

    bugfix children page

--- vv-breadcrumb.php
+++ vv-breadcrumb.php
@@ -12,259 +12,239 @@
  */
 
 
-function name_in_breadcrumb( $name, $positionIndex ) {
-	$str_link = '<span itemprop="name">%1$s</span><meta itemprop="position" content="%2$s" />';
-	return sprintf( $str_link,  $name, $positionIndex );
+function name_in_breadcrumb($name, $positionIndex)
+{
+    $str_link = '<span itemprop="name">%1$s</span><meta itemprop="position" content="%2$s" />';
+    return sprintf($str_link, $name, $positionIndex);
 }
 
-function link_in_breadcrumb( $name, $positionIndex, $link ) {
-	$str_link = '<a href="%1$s" itemprop="item" title="%2$s"><span itemprop="name">%2$s</span></a><meta itemprop="position" content="%3$s" />';
+function link_in_breadcrumb($name, $positionIndex, $link)
+{
+    $str_link = '<a href="%1$s" itemprop="item" title="%2$s"><span itemprop="name">%2$s</span></a><meta itemprop="position" content="%3$s" />';
 
-	return sprintf( $str_link, $link, $name, $positionIndex );
+    return sprintf($str_link, $link, $name, $positionIndex);
 }
 
-function list_in_breadcrumb( $name, $positionIndex, $link = false, $class = false ) {
-	$list = '<li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem"';
+function list_in_breadcrumb($name, $positionIndex, $link = false, $class = false)
+{
+    $list = '<li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem"';
 
-	$list .= $class ? 'class="vv-breadcrumb__item ' . $class . '">' : 'class="vv-breadcrumb__item">';
+    $list .= $class ? 'class="vv-breadcrumb__item ' . $class . '">' : 'class="vv-breadcrumb__item">';
 
-	$list .= $link ? link_in_breadcrumb( $name, $positionIndex, $link ) : name_in_breadcrumb( $name, $positionIndex );
+    $list .= $link ? link_in_breadcrumb($name, $positionIndex, $link) : name_in_breadcrumb($name, $positionIndex);
 
-	$list .= '</li>';
+    $list .= '</li>';
 
-	return $list;
+    return $list;
 }
 
-function vv_breadcrumb() {
+function vv_breadcrumb()
+{
 
-	$positionIndex = 1;
-	// Get text domain for translations
-	$theme       = wp_get_theme();
-	$text_domain = $theme->get( 'TextDomain' );
+    $positionIndex = 1;
+    // Get text domain for translations
+    $theme = wp_get_theme();
+    $text_domain = $theme->get('TextDomain');
 
-	// Open list
-	$breadcrumb = '<ul itemscope itemtype="https://schema.org/BreadcrumbList" id="breadcrumb" class="vv-breadcrumb">';
+    // Open list
+    $breadcrumb = '<ul itemscope itemtype="https://schema.org/BreadcrumbList" id="breadcrumb" class="vv-breadcrumb">';
 
-	// Front page
-	if ( is_front_page() ) {
-		$breadcrumb .= list_in_breadcrumb( get_bloginfo( 'name' ), $positionIndex );
-	} else {
-		$breadcrumb .= list_in_breadcrumb( get_bloginfo( 'name' ), $positionIndex, home_url() );
-	}
-	$positionIndex ++;
+    // Front page
+    if (is_front_page()) {
+        $breadcrumb .= list_in_breadcrumb(get_bloginfo('name'), $positionIndex);
+    } else {
+        $breadcrumb .= list_in_breadcrumb(get_bloginfo('name'), $positionIndex, home_url());
+    }
+    $positionIndex++;
 
-	// Category, tag, author and date archives
-	if ( is_archive() && ! is_tax() && ! is_post_type_archive() ) {
-		$breadcrumb .= '<li class="is-active" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem"><span itemprop="name">';
+    // Category, tag, author and date archives
+    if (is_archive() && !is_tax() && !is_post_type_archive()) {
 
-		// Title of archive
-		if ( is_category() ) {
-			$breadcrumb .= single_cat_title( '', false );
-		} else if ( is_tag() ) {
-			$breadcrumb .= single_tag_title( '', false );
-		} else if ( is_author() ) {
-			$breadcrumb .= get_the_author();
-		} else if ( is_date() ) {
-			if ( is_day() ) {
-				$breadcrumb .= get_the_time( 'F j, Y' );
-			} else if ( is_month() ) {
-				$breadcrumb .= get_the_time( 'F, Y' );
-			} else if ( is_year() ) {
-				$breadcrumb .= get_the_time( 'Y' );
-			}
-		}
+        // Title of archive
+        if (is_category() or is_tag()) {
+            $breadcrumb .= list_in_breadcrumb(single_cat_title('', false), $positionIndex++);
+        } else if (is_author()) {
+            $breadcrumb .= list_in_breadcrumb(get_the_author(), $positionIndex++);
+        } else if (is_date()) {
+            if (is_day()) {
+                $breadcrumb .= list_in_breadcrumb(get_the_time('F j, Y'), $positionIndex++);
+            } else if (is_month()) {
+                $breadcrumb .= list_in_breadcrumb(get_the_time('F, Y'), $positionIndex++);
+            } else if (is_year()) {
+                $breadcrumb .= list_in_breadcrumb(get_the_time('Y'), $positionIndex++);
+            }
+        }
+    } // Posts
+    else if (is_singular('post')) {
 
-		$breadcrumb .= '</span><meta itemprop="position" content="' . $positionIndex . '" /></li>';
-		$positionIndex ++;
-	}
+        // Post categories
+        $post_cats = get_the_category();
 
-	// Posts
-	if ( is_singular( 'post' ) ) {
+        if ($post_cats[0]) {
+            $breadcrumb .= list_in_breadcrumb($post_cats[0]->name, $positionIndex, get_category_link($post_cats[0]->term_id));
+            $positionIndex++;
+        }
 
-		// Post categories
-		$post_cats = get_the_category();
+        // Post title
+        $breadcrumb .= list_in_breadcrumb(get_the_title(), $positionIndex, false, 'is-active');
+        $positionIndex++;
+    } // Pages
+    else if (is_page() && !is_front_page()) {
+        $post = get_post(get_the_ID());
 
-		if ( $post_cats[0] ) {
-			$breadcrumb .= list_in_breadcrumb( $post_cats[0]->name, $positionIndex, get_category_link( $post_cats[0]->term_id ) );
-			$positionIndex ++;
-		}
+        // Page parents
+        if ($post->post_parent) {
+            $parent_id = $post->post_parent;
+            $crumbs = array();
 
-		// Post title
-		$breadcrumb .= list_in_breadcrumb( get_the_title(), $positionIndex, false, 'is-active' );
-		$positionIndex ++;
-	}
+            while ($parent_id) {
+                $page = get_page($parent_id);
+                $crumbs[] = list_in_breadcrumb(get_the_title($page->ID), $positionIndex++, get_permalink($page->ID));
+                $parent_id = $page->post_parent;
+            }
 
-	// Pages
-	if ( is_page() && ! is_front_page() ) {
-		$post = get_post( get_the_ID() );
+            $crumbs = array_reverse($crumbs);
 
-		// Page parents
-		if ( $post->post_parent ) {
-			$parent_id = $post->post_parent;
-			$crumbs    = array();
+            foreach ($crumbs as $crumb) {
+                $breadcrumb .= $crumb;
+            }
+        }
 
-			while ( $parent_id ) {
-				$page      = get_page( $parent_id );
-				$crumbs[]  = list_in_breadcrumb( get_the_title( $page->ID ), $positionIndex, get_permalink( $page->ID ) );
-				$parent_id = $page->post_parent;
-				$positionIndex ++;
-			}
+        // Page title
+        $breadcrumb .= list_in_breadcrumb(get_the_title(), $positionIndex++, false, 'is-active');
+    }
 
-			$crumbs = array_reverse( $crumbs );
+    // Attachments
+    if (is_attachment()) {
+        // Attachment parent
+        $post = get_post(get_the_ID());
 
-			foreach ( $crumbs as $crumb ) {
-				$breadcrumb .= link_in_breadcrumb( $crumb, $positionIndex );
-				$positionIndex ++;
-			}
-		}
+        if ($post->post_parent) {
+            $breadcrumb .= list_in_breadcrumb(get_the_title($post->post_parent), $positionIndex++, get_permalink($post->post_parent));
+        }
 
-		// Page title
-		$breadcrumb .= list_in_breadcrumb( get_the_title(), $positionIndex, false, 'is-active' );
-		$positionIndex ++;
-	}
+        // Attachment title
+        $breadcrumb .= list_in_breadcrumb(get_the_title(), $positionIndex++, false, 'is-active');
+    }
 
-	// Attachments
-	if ( is_attachment() ) {
-		// Attachment parent
-		$post = get_post( get_the_ID() );
+    // Search
+    if (is_search()) {
+        $breadcrumb .= list_in_breadcrumb(__('Search', $text_domain), $positionIndex++, false, 'is-active');;
+    }
 
-		if ( $post->post_parent ) {
-			$breadcrumb .= list_in_breadcrumb( get_the_title( $post->post_parent ), $positionIndex, get_permalink( $post->post_parent ) );
-			$positionIndex ++;
-		}
+    // 404
+    if (is_404()) {
+        $breadcrumb .= list_in_breadcrumb(__('404', $text_domain), $positionIndex++, false, 'is-active');
+    }
 
-		// Attachment title
-		$breadcrumb .= list_in_breadcrumb( get_the_title(), $positionIndex, false, 'is-active' );
-		$positionIndex ++;
-	}
+    // Custom Post Type Archive
+    if (is_post_type_archive()) {
+        $breadcrumb .= list_in_breadcrumb(post_type_archive_title('', false), $positionIndex++, false, 'is-active');
+    }
 
-	// Search
-	if ( is_search() ) {
-		$breadcrumb .= list_in_breadcrumb( __( 'Search', $text_domain ), $positionIndex, false, 'is-active' );;
-		$positionIndex ++;
-	}
+    // Custom Taxonomies
+    if (is_tax()) {
+        // Get the post types the taxonomy is attached to
+        $current_term = get_queried_object();
 
-	// 404
-	if ( is_404() ) {
-		$breadcrumb .= list_in_breadcrumb( __( '404', $text_domain ), $positionIndex, false, 'is-active' );
-		$positionIndex ++;
-	}
+        $attached_to = array();
+        $post_types = get_post_types();
 
-	// Custom Post Type Archive
-	if ( is_post_type_archive() ) {
-		$breadcrumb .= list_in_breadcrumb( post_type_archive_title( '', false ), $positionIndex, false, 'is-active' );
-		$positionIndex ++;
-	}
+        foreach ($post_types as $post_type) {
+            $taxonomies = get_object_taxonomies($post_type);
 
-	// Custom Taxonomies
-	if ( is_tax() ) {
-		// Get the post types the taxonomy is attached to
-		$current_term = get_queried_object();
+            if (in_array($current_term->taxonomy, $taxonomies)) {
+                $attached_to[] = $post_type;
+            }
+        }
 
-		$attached_to = array();
-		$post_types  = get_post_types();
+        // Post type archive link
+        $output = false;
 
-		foreach ( $post_types as $post_type ) {
-			$taxonomies = get_object_taxonomies( $post_type );
+        foreach ($attached_to as $post_type) {
+            $cpt_obj = get_post_type_object($post_type);
 
-			if ( in_array( $current_term->taxonomy, $taxonomies ) ) {
-				$attached_to[] = $post_type;
-			}
-		}
+            if (!$output && get_post_type_archive_link($cpt_obj->name)) {
+                $breadcrumb .= list_in_breadcrumb($cpt_obj->labels->name, $positionIndex++, get_post_type_archive_link($cpt_obj->name));
+                $output = true;
+            }
+        }
 
-		// Post type archive link
-		$output = false;
+        // Term title
+        $breadcrumb .= list_in_breadcrumb(single_term_title('', false), $positionIndex++, false, 'is-active');
+    }
 
-		foreach ( $attached_to as $post_type ) {
-			$cpt_obj = get_post_type_object( $post_type );
+    // Custom Post Types
+    if (is_single() && get_post_type() != 'post' && get_post_type() != 'attachment') {
+        $cpt_obj = get_post_type_object(get_post_type());
 
-			if ( ! $output && get_post_type_archive_link( $cpt_obj->name ) ) {
-				$breadcrumb .= list_in_breadcrumb( $cpt_obj->labels->name, $positionIndex, get_post_type_archive_link( $cpt_obj->name ) );
-				$output     = true;
-				$positionIndex ++;
-			}
-		}
+        // Is cpt hierarchical like pages or posts?
+        if (is_post_type_hierarchical($cpt_obj->name)) {
+            // Like pages
 
-		// Term title
-		$breadcrumb .= list_in_breadcrumb( single_term_title( '', false ), $positionIndex, false, 'is-active' );
-		$positionIndex ++;
-	}
+            // Cpt archive
+            if (get_post_type_archive_link($cpt_obj->name)) {
+                $breadcrumb .= list_in_breadcrumb($cpt_obj->labels->name, $positionIndex++, get_post_type_archive_link($cpt_obj->name));
+            }
 
-	// Custom Post Types
-	if ( is_single() && get_post_type() != 'post' && get_post_type() != 'attachment' ) {
-		$cpt_obj = get_post_type_object( get_post_type() );
+            // Cpt parents
+            $post = get_post(get_the_ID());
 
-		// Is cpt hierarchical like pages or posts?
-		if ( is_post_type_hierarchical( $cpt_obj->name ) ) {
-			// Like pages
+            if ($post->post_parent) {
+                $parent_id = $post->post_parent;
+                $crumbs = array();
 
-			// Cpt archive
-			if ( get_post_type_archive_link( $cpt_obj->name ) ) {
-				$breadcrumb .= list_in_breadcrumb( $cpt_obj->labels->name, $positionIndex, get_post_type_archive_link( $cpt_obj->name ) );
-				$positionIndex ++;
-			}
+                while ($parent_id) {
+                    $page = get_page($parent_id);
+                    $crumbs[] = link_in_breadcrumb(get_the_title($page->ID), $positionIndex++, get_permalink($page->ID));
+                    $parent_id = $page->post_parent;
+                }
 
-			// Cpt parents
-			$post = get_post( get_the_ID() );
+                $crumbs = array_reverse($crumbs);
 
-			if ( $post->post_parent ) {
-				$parent_id = $post->post_parent;
-				$crumbs    = array();
+                foreach ($crumbs as $crumb) {
+                    $breadcrumb .= list_in_breadcrumb($crumb, $positionIndex++);
+                }
+            }
+        } else {
+            // Like posts
 
-				while ( $parent_id ) {
-					$page      = get_page( $parent_id );
-					$crumbs[]  = link_in_breadcrumb( get_the_title( $page->ID ), $positionIndex, get_permalink( $page->ID ) );
-					$parent_id = $page->post_parent;
-					$positionIndex ++;
-				}
+            // Cpt archive
+            if (get_post_type_archive_link($cpt_obj->name)) {
+                $breadcrumb .= list_in_breadcrumb($cpt_obj->labels->name, $positionIndex, get_post_type_archive_link($cpt_obj->name));
+            }
 
-				$crumbs = array_reverse( $crumbs );
+            // Get cpt taxonomies
+            $cpt_taxes = get_object_taxonomies($cpt_obj->name);
 
-				foreach ( $crumbs as $crumb ) {
-					$breadcrumb .= list_in_breadcrumb( $crumb, $positionIndex );
-					$positionIndex ++;
-				}
-			}
-		} else {
-			// Like posts
+            if ($cpt_taxes && is_taxonomy_hierarchical($cpt_taxes[0])) {
+                // Other taxes attached to the cpt could be hierachical, so need to look into that.
+                $cpt_terms = get_the_terms(get_the_ID(), $cpt_taxes[0]);
 
-			// Cpt archive
-			if ( get_post_type_archive_link( $cpt_obj->name ) ) {
-				$breadcrumb .= list_in_breadcrumb( $cpt_obj->labels->name, $positionIndex, get_post_type_archive_link( $cpt_obj->name ) );
-			}
+                if (is_array($cpt_terms)) {
+                    $output = false;
 
-			// Get cpt taxonomies
-			$cpt_taxes = get_object_taxonomies( $cpt_obj->name );
+                    foreach ($cpt_terms as $cpt_term) {
+                        if (!$output) {
+                            $breadcrumb .= list_in_breadcrumb($cpt_term->name, $positionIndex++, get_term_link($cpt_term->name, $cpt_taxes[0]));
+                            $output = true;
+                        }
+                    }
+                }
+            }
+        }
 
-			if ( $cpt_taxes && is_taxonomy_hierarchical( $cpt_taxes[0] ) ) {
-				// Other taxes attached to the cpt could be hierachical, so need to look into that.
-				$cpt_terms = get_the_terms( get_the_ID(), $cpt_taxes[0] );
+        // Cpt title
+        $breadcrumb .= list_in_breadcrumb(get_the_title(), $positionIndex, false, 'is-active');
+    }
 
-				if ( is_array( $cpt_terms ) ) {
-					$output = false;
+    // Close list
+    $breadcrumb .= '</ul>';
 
-					foreach ( $cpt_terms as $cpt_term ) {
-						if ( ! $output ) {
-							$breadcrumb .= list_in_breadcrumb( $cpt_term->name, $positionIndex, get_term_link( $cpt_term->name, $cpt_taxes[0] ) );
-							$positionIndex ++;
-							$output = true;
-						}
-					}
-				}
-			}
-		}
-
-		// Cpt title
-		$breadcrumb .= list_in_breadcrumb( get_the_title(), $positionIndex, false, 'is-active' );
-	}
-
-	// Close list
-	$breadcrumb .= '</ul>';
-
-	// Ouput
-	echo $breadcrumb;
+    // Ouput
+    echo $breadcrumb;
 
 }
 
-add_shortcode( 'vv_breadcrumb', 'vv_breadcrumb' );
+add_shortcode('vv_breadcrumb', 'vv_breadcrumb');
 ?>
